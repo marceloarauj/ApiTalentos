d50bd80c459ef6e1c0f56cb460b8cddfe7554632
marceloarauj <42809145+marceloarauj@users.noreply.github.com>
2019-10-06 17:55:39 -0300

    Ts

--- api.py
+++ api.py
@@ -13,4 +13,4 @@ def hello():
     return "[{'name': ' Atletismo - Arremesso de Peso ','id':1},{'name': ' Atletismo - Corrida de Velocidade ','id':2},{'name': ' Atletismo - Corrida de Fundo ','id':3},{'name': ' Atletismo - Corridas de Meio-Fundo','id':4},]"
 
 if __name__ == "__main__":
-    app.run(port=7000)
+    app.run()
